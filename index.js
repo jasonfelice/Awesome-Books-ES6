7bb45cbee993d2c19b1bce005df8d0cba756e165
Jason Felice <jasonfelice0@gmail.com>
2022-04-25 03:28:51 -0700

    Add validation and messages in the add books form

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import Book from './modules/Books.js';
 import updateBooks from './modules/update-books.js';
 import { DateTime } from "./modules/luxon.js";
 
+const errorMessage = document.querySelector('.message-prompt');
 const now = DateTime.now();
 const date = document.querySelector('.date');
 date.textContent = now.toLocaleString(DateTime.DATETIME_MED) ;
@@ -22,7 +23,16 @@ if (localStorage.books) {
   }
   
   addButton.addEventListener('click', () => {
+    if (inputTitle.value && inputAuthor.value) {
     const currentObj = new Book(inputTitle.value, inputAuthor.value);
     currentObj.addBook();
     updateBooks();
+    errorMessage.textContent = "The book has been added!";
+    errorMessage.classList.add('success');
+    }
+    else if(!inputTitle.value && !inputAuthor.value) {
+      errorMessage.textContent = "Please fill the form!";
+      errorMessage.classList.add('error');
+    }
+    
   });
